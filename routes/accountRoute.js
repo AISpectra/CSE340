2edf006d787700e6d80ed1521266d58b2c03ab2f
Samuel <spectra.gethelp@gmail.com>
2025-11-20 22:34:27 +0100

    all

---
+++ routes/accountRoute.js
@@ -0,0 +1,33 @@
+
+// Needed resources
+const express = require("express")
+const router = express.Router()
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation")
+
+
+// Route for login view
+router.get(
+  "/login",
+  utilities.handleErrors(accountController.buildLogin)
+)
+
+// Route to build registration view
+router.get(
+  "/register",
+  utilities.handleErrors(accountController.buildRegister)
+)
+
+
+// Process registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -13,8 +13,41 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities")
+const session = require("express-session")
+const pool = require("./database/")
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
 
 
+
+
+
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require("connect-pg-simple")(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: "sessionId",
+}))
+
+// Express Messages Middleware
+app.use(require("connect-flash")())
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 
@@ -30,6 +63,8 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
+app.use("/account", accountRoute)
+
 
 // Error test route (Task 3, la vemos abajo)
 app.get(
